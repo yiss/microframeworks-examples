968ad420861571c527bce17eabb30dda642aa309
Ilias YAHIA <ilias.yahia1@gmail.com>
2018-06-06 13:06:33 +0200

    Added Javalin Java project

---
+++ javalin-java-example/src/main/java/io/yiss/JavalinApp.java
@@ -0,0 +1,10 @@
+package io.yiss;
+
+import io.javalin.*;
+
+public class JavalinApp {
+    public static void main(String[] args) {
+        Javalin app = Javalin.start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
